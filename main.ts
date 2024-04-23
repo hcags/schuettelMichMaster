ab1ab0e11ff98f0740e50a5d830e4a972f6b907d
hcags <120849679+hcags@users.noreply.github.com>
2024-04-23 04:56:44 -0700

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    if (true) {
+    	
+    }
+})
+let gamerLives: number[] = []
+let gameStart = false
+radio.setGroup(1)
+gamerLives[1] = 5
+gamerLives[2] = 5
+gamerLives[3] = 5
+gamerLives[4] = 5
+gamerLives[5] = 5
+basic.showIcon(IconNames.Asleep)
