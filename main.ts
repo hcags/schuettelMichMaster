f216e5f14e1961f59f88e690e8d400b5ee9e2b07
hcags <120849679+hcags@users.noreply.github.com>
2024-04-23 14:32:56 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    if (true) {
-    	
+    if (!(gameStart)) {
+        gameStart = true
+        radio.sendString("gameStart")
     }
 })
+function setLive (gamer: number, Live: number, ledOn: boolean) {
+	
+}
 let gamerLives: number[] = []
 let gameStart = false
+gameStart = false
 radio.setGroup(1)
 gamerLives[1] = 5
 gamerLives[2] = 5
