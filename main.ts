1235c4ac88709d21d10a950e0e5b79b4cd6f17ff
hcags <120849679+hcags@users.noreply.github.com>
2024-05-28 10:49:14 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,35 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    currentLives = gamerLives[receivedNumber]
+    if (currentLives > 0) {
+        currentLives += -1
+        gamerLives[receivedNumber] = currentLives
+    }
+    led.unplot(receivedNumber - 1, currentLives)
+})
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    if (true) {
-    	
+    let gameStart = 0
+    if (!(gameStart)) {
+        backTimer = 5
+        basic.showNumber(backTimer)
+        basic.pause(1000)
+        while (backTimer > 0) {
+            backTimer += -1
+            basic.showNumber(backTimer)
+            basic.pause(1000)
+        }
+        radio.sendString("gameStart")
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
     }
 })
+let backTimer = 0
+let currentLives = 0
 let gamerLives: number[] = []
-let gameStart = false
 radio.setGroup(1)
 gamerLives[1] = 5
 gamerLives[2] = 5
